5708800a3a55085efd893db7e53d9fdf36316576
Will Jacobs <willjjacobs@gmail.com>
2017-10-27 10:50:30 -0500

    Moved around UI code a bit in order to be able to call from top level. Currently using one global, could be worked around.

--- dora/dashboard/UI_Start.py
+++ dora/dashboard/UI_Start.py
@@ -2,19 +2,19 @@
 # -*- coding: utf-8 -*-
 #Test
 """
-ZetCode PyQt5 tutorial 
+ZetCode PyQt5 tutorial
 
 In this example, we create a simple
 window in PyQt5.
 
 Author: Jan Bodnar
-Website: zetcode.com 
+Website: zetcode.com
 Last edited: August 2017
 """
 
 import sys
-from PyQt5.QtWidgets import (QMainWindow, QAction, QTabWidget, 
-                             QVBoxLayout, QHBoxLayout, qApp, QApplication, 
+from PyQt5.QtWidgets import (QMainWindow, QAction, QTabWidget,
+                             QVBoxLayout, QHBoxLayout, qApp, QApplication,
                              QWidget, QLineEdit, QPushButton, QMessageBox)
 from PyQt5.QtCore import QCoreApplication, pyqtSlot
 from PyQt5.QtGui import QIcon
@@ -22,86 +22,86 @@ from PyQt5.QtGui import QFont
 
 
 class Window(QMainWindow):
-    
+
     def __init__(self):
         super().__init__()
-        
+
         self.initUI()
-        
-    
-        
+
+
+
     def initUI(self):
         self.table_widget = tabWidget(self)
         self.setCentralWidget(self.table_widget)
-        
+
         menubar = self.menuBar() #Create Menu Bar
         doraMenu = menubar.addMenu('&DORA') #Create DORA Menu
-        
+
         hardwareAct = QAction('&Hardware', self) #Add Hardware to DORA
         doraMenu.addAction(hardwareAct)
-        
+
         aboutAct = QAction('&About', self) #Add About to DORA
         doraMenu.addAction(aboutAct)
-        
+
         creditsAct = QAction('&Credits', self) #Add Credits to DORA
         doraMenu.addAction(creditsAct)
-        
-        
+
+
         fileMenu = menubar.addMenu('&File') #Create File Menu
-        
+
         loadHardwareProfileAct = QAction('&Load Hardware Profile', self)
         fileMenu.addAction(loadHardwareProfileAct)
-        
+
         loadImageAct = QAction('&Load Image', self)
         fileMenu.addAction(loadImageAct)
-        
+
         load_video_stream_act = QAction('&Load Video Stream', self)
         fileMenu.addAction(load_video_stream_act)
-        
+
         load_neural_net_act = QAction('&Load Neural Net', self)
         fileMenu.addAction(load_neural_net_act)
-        
+
         save_image_act = QAction('&Save Image', self)
         fileMenu.addAction(save_image_act)
-        
+
         exitAct = QAction('&Exit', self)
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('Exit application')
         exitAct.triggered.connect(app.quit)
         fileMenu.addAction(exitAct)
-        
-        
+
+
         windowMenu = menubar.addMenu('&Window') #Create Window Menu
-        
+
         RGB_visual_act = QAction('&RGB', self)
         windowMenu.addAction(RGB_visual_act)
-        
+
         greyscale_visual_act = QAction('Greyscale', self)
         windowMenu.addAction(greyscale_visual_act)
-        
+
         depthmap_visual_act = QAction('Depth Map', self)
         windowMenu.addAction(depthmap_visual_act)
-        
+
         datatable_act = QAction('Data Table', self)
         windowMenu.addAction(datatable_act)
-        
-        
+
+
         settingsMenu = menubar.addMenu('&Settings') #Create Settings Menu
-       
+
         preferences_act = QAction('&Preferences', self)
         settingsMenu.addAction(preferences_act)
-        
+
         preprocessing_act = QAction('&Pre=Processing', self)
         settingsMenu.addAction(preprocessing_act)
-        
-        
+
+
         self.setGeometry(960,100,960,540)
         self.setWindowTitle('ICON')
         #self.setWindowIcon(QIcon('web.png'))
-        
+
         self.show()
-        
-        
+
+
 class tabWidget(QWidget):
 
     def __init__(self, parent):
@@ -111,16 +111,16 @@ class tabWidget(QWidget):
         self.setGeometry
           # Initialize tab screen
         self.tabs = QTabWidget()
-        self.tab_tools = QWidget()	
+        self.tab_tools = QWidget()
         self.tab_console = QWidget()
         self.tab_log = QWidget()
-        self.tabs.resize(300,200) 
- 
+        self.tabs.resize(300,200)
+
         # Add tabs
         self.tabs.addTab(self.tab_tools,"Tools")
         self.tabs.addTab(self.tab_console,"Console")
         self.tabs.addTab(self.tab_log,"Log")
- 
+
         # Create Tools tab
         self.tab_tools.vlayout01 = QVBoxLayout(self)
         self.tab_tools.vlayout02 = QVBoxLayout(self)
@@ -139,35 +139,35 @@ class tabWidget(QWidget):
         self.tab_tools.hlayout.addLayout(self.tab_tools.vlayout01)
         self.tab_tools.hlayout.addLayout(self.tab_tools.vlayout02)
         self.tab_tools.setLayout(self.tab_tools.hlayout)
- 
+
         # Create Console tab
         #Setup Layout
         self.tab_console.layout = QVBoxLayout(self)
         self.tab_console.layout.addStretch(1)
         #self.console_input.move(0,0)
         self.tab_console.setLayout(self.tab_console.layout)
-       
+
         #Console Input fiel
         self.console_input = QLineEdit(self)
         self.console_input.returnPressed.connect(self.on_command)
-        
-        
+
+
         #Add widgets to tab
         self.tab_console.layout.addWidget(self.console_input)
-        
+
         # Create logs tab
         # TODO
-        
-        # Add tabs to widget        
+
+        # Add tabs to widget
         self.layout.addWidget(self.tabs)
         self.setLayout(self.layout)
- 
+
     @pyqtSlot()
     def on_command(self):
         print("\n")
         print(self.console_input.text())
         self.console_input.setText("")
-    
+
     @pyqtSlot()
     def on_click(self):
         print("\n")
@@ -175,17 +175,17 @@ class tabWidget(QWidget):
         for currentQTableWidgetItem in self.tableWidget.selectedItems():
             print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
 
-        
-    
-        
-        
-        
-        
-        
-        
+
+def ui_main():
+  global app # make available elsewhere - only need to declare global if we assign
+  app = QApplication(sys.argv)
+  app.aboutToQuit.connect(app.deleteLater)
+  ex = Window()
+  sys.exit(app.exec_())
+
 if __name__ == '__main__':
-    
-    app = QApplication(sys.argv)
-    app.aboutToQuit.connect(app.deleteLater)
-    ex = Window()
-    sys.exit(app.exec_())
+    # ui_main()
+  app = QApplication(sys.argv)
+  app.aboutToQuit.connect(app.deleteLater)
+  ex = Window()
+  sys.exit(app.exec_())
