ef39152dace53238cbcd3f682395c162eb20adc8
Yazeed Sabri <ysabri@wisc.edu>
2017-10-31 19:22:59 -0500

    deleted the getters and setters in NNDTO file

--- dora/core/NeuralNet/NeuralNetDTO.py
+++ dora/core/NeuralNet/NeuralNetDTO.py
@@ -6,27 +6,9 @@ class NeuralNetDTO:
     scores = None;
     distance = None;
 
-    def __init__(self, boxes, category_index, 
+    def __init__(self, boxes, category_index,
     	classes, scores):
         self.boxes = boxes
         self.category_index = category_index
         self.classes = classes
         self.scores = scores
-
-
-
-    def getBoxes(self):
-    	return self.boxes
-
-    def getScores(self):
-    	return self.scores
-
-    def getIndex(self)
-    	return self.category_index
-
-    def setDitance(self, dist)
-    	self.distance = dist
-
-    def getDistance(self)
-    	return self.distance
-
